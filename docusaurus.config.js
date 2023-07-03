981df4e63565814f32d84ddf297e0de3ca12fe42
ahKevinXy <2502329620@qq.com>
2023-07-03 17:15:32 +0800

    修复检查路径

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://goofish-shop.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/goofish-shop-video/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
